f9bd71c26cac8f8b729a7569c90f844c5829983c
PuckSolo <skulchai.ch@gmail.com>
2020-11-21 18:29:11 +0700

    Post working

--- src/index.js
+++ src/index.js
@@ -5,24 +5,24 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.min.js';
-//import { Provider } from 'react-redux';
-//import { createStore, applyMiddleware  } from 'redux';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware  } from 'redux';
 // import { BrowserRouter } from 'react-router-dom';
 // import AppReducers from './reducers';
 // import thunk from 'redux-thunk';
 
-let initialState = {};
-const appStore = createStore(
-    AppReducers,
-    //Browser addon Redux Devtools
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    //applyMiddleware(thunk)
-    );
+// let initialState = {};
+// const appStore = createStore(
+//    AppReducers,
+//    //Browser addon Redux Devtools
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//    //applyMiddleware(thunk)
+//    );
 
 ReactDOM.render(
-    //<Provider store={appStore}>
+  //  <Provider store={appStore}>
         <App />,
-    //</Provider>,
+  //  </Provider>,
     
 document.getElementById('app_root'));
 
